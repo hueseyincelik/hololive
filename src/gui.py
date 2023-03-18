e54b7f034fc24e53225aa3df7071afc48a955152
hueseyincelik <104273133+hueseyincelik@users.noreply.github.com>
2023-03-18 19:23:25 +0100

    implement sideband locking and quadrant switching

--- src/gui.py
+++ src/gui.py
@@ -18,7 +18,6 @@ class GUI:
         port,
         camera="CCD",
         exposure_time=0.2,
-        sideband_quadrant="upper_left",
         auto_correlation_buffer=50,
         dimension=512,
     ):
@@ -27,10 +26,11 @@ class GUI:
         self.microscope = microscope.Microscope(ip, port)
         self.microscope.configure_camera(camera, exposure_time)
 
-        self.sideband_quadrant, self.auto_correlation_buffer = (
-            sideband_quadrant,
-            auto_correlation_buffer,
-        )
+        self.auto_correlation_buffer = auto_correlation_buffer
+
+        self.sideband_quadrant = "upper_left"
+        self.sideband_position, self.sideband_distance = (0, 0), 0
+        self.sideband_lock = False
 
         pg.init()
 
@@ -44,6 +44,18 @@ class GUI:
             for event in pg.event.get():
                 if event.type == pg.QUIT:
                     sys.exit()
+                elif event.type == pg.KEYDOWN:
+                    if event.key == pg.K_l:
+                        self.sideband_lock = not self.sideband_lock
+
+                    if event.key == pg.K_LEFT:
+                        self.sideband_quadrant = "lower_left"
+                    if event.key == pg.K_RIGHT:
+                        self.sideband_quadrant = "upper_right"
+                    if event.key == pg.K_UP:
+                        self.sideband_quadrant = "upper_left"
+                    if event.key == pg.K_DOWN:
+                        self.sideband_quadrant = "lower_right"
 
             current_phase = self.get_phase()
             current_phase_grayscale = self.grayscale_convert(
@@ -81,23 +93,23 @@ class GUI:
                 img_fft_shifted.shape[0] // 2 + self.auto_correlation_buffer :,
                 img_fft_shifted.shape[1] // 2 + self.auto_correlation_buffer :,
             ]
-        else:
-            raise ValueError("Unsupported position!")
 
-        sideband_position = np.argwhere(img_fft_shifted == np.amax(img_shift_cropped))[
-            0
-        ]
-
-        sideband_distance = np.linalg.norm(
-            np.asarray([int(p / 2) - 1 for p in img_shift_cropped.shape[::-1]])
-            - np.asarray(sideband_position[::-1])
-        )
+        if not self.sideband_lock:
+            self.sideband_position = np.argwhere(
+                img_fft_shifted == img_shift_cropped.max()
+            )[0]
+            self.sideband_distance = np.linalg.norm(
+                np.asarray([int(p / 2) - 1 for p in img_shift_cropped.shape[::-1]])
+                - np.asarray(self.sideband_position[::-1])
+            )
 
         img_cut_out = img_fft_shifted[
-            sideband_position[0] - int(sideband_distance / 6) : sideband_position[0]
-            + int(sideband_distance / 6),
-            sideband_position[1] - int(sideband_distance / 6) : sideband_position[1]
-            + int(sideband_distance / 6),
+            self.sideband_position[0]
+            - int(self.sideband_distance / 6) : self.sideband_position[0]
+            + int(self.sideband_distance / 6),
+            self.sideband_position[1]
+            - int(self.sideband_distance / 6) : self.sideband_position[1]
+            + int(self.sideband_distance / 6),
         ]
 
         padding = np.abs(img_cut_out.shape[0] - self.dimension) // 2
