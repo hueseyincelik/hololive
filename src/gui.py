eee1a001e332a3b6aea2de315e6ac9137689393c
hueseyincelik <104273133+hueseyincelik@users.noreply.github.com>
2023-03-22 17:20:05 +0100

    wrap amplified phase image back to [-pi, pi]

--- src/gui.py
+++ src/gui.py
@@ -64,7 +64,7 @@ class GUI:
 						self.save_screenshot_thread = Thread(target=self.save_screenshot)
 						self.save_screenshot_thread.start()
 
-			self.current_phase = self.phase_amplification * self.get_phase()
+			self.current_phase = np.angle(np.exp(1j * self.phase_amplification * self.get_phase())) if self.phase_amplification != 1 else self.get_phase()
 			self.current_phase_grayscale = self.grayscale_convert(255 * self.current_phase / self.current_phase.max())
 
 			surface_phase_image = pg.surfarray.make_surface(self.current_phase_grayscale)
