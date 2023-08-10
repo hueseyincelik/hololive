23ed07620f7c229cf4340fbf8c6c83af59d9eb7b
hueseyincelik <104273133+hueseyincelik@users.noreply.github.com>
2023-08-10 13:58:42 +0200

    reset camera settings when quitting

--- src/microscope.py
+++ src/microscope.py
@@ -5,6 +5,10 @@ class Microscope:
 		self.ip, self.port = ip, port
 		self.microscope = temscript.RemoteMicroscope((self.ip, self.port)) if remote else temscript.Microscope()
 
+	def __del__(self):
+		if self.camera is not None:
+			self.microscope.set_camera_param(self.camera, {'exposure(s)': 4.0, 'binning': 1})
+
 	def configure_camera(self, camera, exposure_time, binning):
 		self.camera, self.exposure_time, self.binning = camera, exposure_time, binning
 		self.microscope.set_camera_param(self.camera, {'exposure(s)': self.exposure_time, 'binning': self.binning})
