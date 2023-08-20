ead981200726b9cdf0d0491adc17e99dfc8fe6d8
hueseyincelik <104273133+hueseyincelik@users.noreply.github.com>
2023-08-20 17:46:57 +0200

    implement subtraction of reference hologram

--- README.md
+++ README.md
@@ -5,6 +5,8 @@ Live phase and amplitude reconstruction for off-axis electron holography with FE
 
 The retrieval of the $2\pi$-wrapped phase image works by taking the detector readout, Fourier transforming it such that the autocorrelation lies in the center, automatically finding the sideband in the specified image quadrant, centering a cut-out around the sideband with a width of $1/3$ of the distance between the autocorrelation and the sideband through zero padding in Fourier space and calculating the angle of the complex argument of the inverse Fourier transform. The reconstructed amplitude in turn is defined as the the absolute value of the inverse Fourier transform.
 
+Distortion-induced phase modulations can optionally be corrected using an empty hologram (i.e. without the specimen in the field of view) as a reference hologram.
+
 Estimation of the fringe contrast in Fourier space is defined as twice the amplitude fraction of the sideband and the centerband (i.e. the autocorrelation).
 ## Installation
 Install all required packages with pip using:
@@ -20,12 +22,13 @@ python3 main.py
 In order to remotely connect to the microscope, `temscript` has to be running on the accompanying PC (which in turn requires the TEMScripting software interface from Thermo Fisher Scientific™ and FEI™) and the corresponding IP address and port have to be provided.
 
 Various keyboard shortcuts can be used to adjust some of the parameters used for the live reconstruction:
-- `L` can be used to lock the sideband position, preventing it from being automatically recalculated until the lock is released again
+- `L` can be used to lock the sideband position, preventing it from being automatically recalculated until the lock is released again (note that disabling the sideband lock also resets/disables the reference hologram utilized during the reconstruction process)
 - `+/-` can be used to adjust the buffer around the image quadrant, which is trimmed off in order to avoid the influence of the autocorrelation during the automatic sideband detection, in steps of $5~\text{px}$
 - `UP,DOWN,LEFT,RIGHT` can be used to change the image quadrant in which the sideband is automatically detected
 - `TAB` can be used to switch between the reconstructed phase and amplitude
 - `A` can be used to cycle between the values $1,2,3,4$ as an amplification factor for the reconstructed phase
 - `H` can be used to apply Hann smoothing to the sideband cut-out before zero padding
+- `R` can be used to utilize the current acquisition as a reference hologram during the reconstruction process (note that this also locks the sideband position)
 - `S` can be used to save a screenshot of the currently reconstructed phase/amplitude as a timestamped 32-Bit float TIFF image
 - `P` can be used to pause the acquisition of new phase/amplitude images
 
--- src/gui.py
+++ src/gui.py
@@ -29,6 +29,7 @@ class GUI:
 		self.amplifications, self.phase_amplification = it.islice(it.cycle([1, 2, 3, 4]), 1, None), 1
 		self.auto_correlation_buffer, self.hann_smoothing = 50, True
 
+		self.object_image_wave, self.reference_image_wave = None, None
 		self.fringe_contrast = 0
 
 		self.reconstruct_amplitude = False
@@ -51,6 +52,7 @@ class GUI:
 				elif event.type == pg.KEYDOWN:
 					if event.key == pg.K_l:
 						self.sideband_lock = not self.sideband_lock
+						self.reference_image_wave = None
 
 					if event.key == pg.K_UP:
 						self.sideband_quadrant = 'top'
@@ -73,6 +75,10 @@ class GUI:
 					if event.key == pg.K_a and not self.reconstruct_amplitude:
 						self.phase_amplification = next(self.amplifications)
 
+					if event.key == pg.K_r:
+						self.reference_image_wave = self.object_image_wave.copy()
+						self.sideband_lock = True
+
 					if event.key == pg.K_s:
 						self.save_screenshot_thread = Thread(target=self.save_screenshot)
 						self.save_screenshot_thread.start()
@@ -84,11 +90,14 @@ class GUI:
 						self.hann_smoothing = not self.hann_smoothing
 
 			if not self.pause:
-				self.current_phase = np.angle(np.exp(1j * self.phase_amplification * self.reconstruct())) if self.phase_amplification != 1 and not self.reconstruct_amplitude else self.reconstruct()
-				self.current_phase_grayscale = self.grayscale_convert(255 * self.current_phase / self.current_phase.max())
+				self.current_reconstruction = (np.angle(np.exp(1j * self.phase_amplification * self.reconstruct())) if self.phase_amplification != 1 and not self.reconstruct_amplitude else self.reconstruct()).swapaxes(0, 1)
+				self.current_reconstruction_grayscale = self.grayscale_convert(255 * self.current_reconstruction / self.current_reconstruction.max())
 
-			surface_phase_image = pg.transform.smoothscale(pg.surfarray.make_surface(self.current_phase_grayscale), pg.display.get_surface().get_size())
+			surface_phase_image = pg.transform.smoothscale(pg.surfarray.make_surface(self.current_reconstruction_grayscale), pg.display.get_surface().get_size())
 			self.screen.blit(surface_phase_image, (0, 0))
+
+			if self.reference_image_wave is not None:
+				pg.draw.rect(self.screen, pg.Color('RED'), [0, 0, *pg.display.get_surface().get_size()], 4)
 
 			for coordinate, message in zip([(5, 5), (5, 25), (5, 45), (5, 85), (5, 105)], [f"Smoothing: {self.hann_smoothing}", f"Amplification: {self.phase_amplification}", f"Buffer: {self.auto_correlation_buffer}", f"Sideband: {self.sideband_quadrant}{' (L)' if self.sideband_lock else ''}", f"Contrast: {np.round(100 * self.fringe_contrast, 2)}%"]):
 				self.font.render_to(self.screen, coordinate, message, pg.Color('RED'))
@@ -114,7 +123,7 @@ class GUI:
 			self.sideband_position = np.argwhere(img_fft_shifted == img_shift_cropped.max())[0]
 			self.sideband_distance = np.linalg.norm(np.asarray(img_fft_shifted.shape) / 2 - self.sideband_position)
 
-		self.fringe_contrast = 2 * np.abs(img_fft_shifted[*self.sideband_position]) / np.abs(img_fft_shifted[img_fft_shifted.shape[0] // 2, img_fft_shifted.shape[0] // 2])
+		self.fringe_contrast = 2 * np.abs(img_fft_shifted[tuple(self.sideband_position)]) / np.abs(img_fft[0, 0])
 
 		cut_out_idx = [[int(np.clip(sb_pos - self.sideband_distance / 6, 0, img_dim - 1)), int(np.clip(sb_pos + self.sideband_distance / 6, 0, img_dim - 1))] for (sb_pos, img_dim) in zip(self.sideband_position, img_fft_shifted.shape)]
 
@@ -124,9 +133,11 @@ class GUI:
 				img_cut_out *= window('hann', img_cut_out.shape)
 
 		padding = np.abs(img_cut_out.shape[0] - self.dimension) // 2
-		img_zero_padded = np.pad(img_cut_out, ((padding, padding), (padding, padding)), constant_values=0)
 
-		return np.abs(sfft.ifft2(img_zero_padded)).swapaxes(0, 1) if self.reconstruct_amplitude else np.angle(sfft.ifft2(img_zero_padded)).swapaxes(0, 1)
+		self.object_image_wave = sfft.ifft2(np.pad(img_cut_out, ((padding, padding), (padding, padding)), constant_values=0))
+		reconstructed_image_wave = self.object_image_wave / self.reference_image_wave if self.reference_image_wave is not None else self.object_image_wave
+
+		return np.abs(reconstructed_image_wave) if self.reconstruct_amplitude else np.angle(reconstructed_image_wave)
 
 	def grayscale_convert(self, image):
 		image = 255 * (image / image.max())
@@ -138,4 +149,4 @@ class GUI:
 		return image_gray
 
 	def save_screenshot(self, datatype=np.float32, photometric='minisblack'):
-		imwrite(f"hololive_{format(datetime.now(), '%Y-%m-%d_%H-%M-%S')}.tif", self.current_phase.astype(datatype), photometric=photometric)
+		imwrite(f"hololive_{format(datetime.now(), '%Y-%m-%d_%H-%M-%S')}.tif", self.current_reconstruction.astype(datatype), photometric=photometric)
