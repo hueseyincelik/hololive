a52c4ff4cf546d9641bfc8e709e1c003d1446d1e
hueseyincelik <104273133+hueseyincelik@users.noreply.github.com>
2024-05-04 22:31:08 +0200

    simplify centerband and sideband masking routine

--- src/gui.py
+++ src/gui.py
@@ -1,4 +1,4 @@
-from skimage.filters import window
+from skimage import draw, filters
 import scipy.fft as sfft
 import numpy as np
 
@@ -21,11 +21,10 @@ class GUI:
 		self.microscope.configure_camera(camera, exposure_time, binning)
 
 		self.sideband_position, self.sideband_distance = (0, 0), 0
-		self.sideband_quadrant = 'top'
-		self.sideband_lock = False
+		self.sideband_area, self.sideband_lock = 'upper', False
 
 		self.amplifications, self.phase_amplification = it.islice(it.cycle([1, 2, 3, 4]), 1, None), 1
-		self.auto_correlation_buffer, self.hann_smoothing = 50, True
+		self.centerband_mask, self.hann_smoothing = 5, True
 
 		self.object_image_wave, self.reference_image_wave = None, None
 		self.fringe_contrast = 0
@@ -53,18 +52,14 @@ class GUI:
 						self.reference_image_wave = None
 
 					if event.key == pg.K_UP:
-						self.sideband_quadrant = 'top'
+						self.sideband_area = 'upper'
 					if event.key == pg.K_DOWN:
-						self.sideband_quadrant = 'bottom'
-					if event.key == pg.K_LEFT:
-						self.sideband_quadrant ='left'
-					if event.key == pg.K_RIGHT:
-						self.sideband_quadrant = 'right'
+						self.sideband_area = 'lower'
 
 					if event.key == pg.K_PLUS:
-						self.auto_correlation_buffer += 5
-					if event.key == pg.K_MINUS and self.auto_correlation_buffer >= 5:
-						self.auto_correlation_buffer -= 5
+						self.centerband_mask += 1
+					if event.key == pg.K_MINUS and self.centerband_mask > 1:
+						self.centerband_mask -= 1
 
 					if event.key == pg.K_TAB:
 						self.reconstruct_amplitude = not self.reconstruct_amplitude
@@ -88,7 +83,11 @@ class GUI:
 						self.hann_smoothing = not self.hann_smoothing
 
 			if not self.pause:
-				self.current_reconstruction = (np.angle(np.exp(1j * self.phase_amplification * self.reconstruct())) if self.phase_amplification != 1 and not self.reconstruct_amplitude else self.reconstruct()).swapaxes(0, 1)
+				self.current_reconstruction =  self.reconstruct()
+
+				if self.phase_amplification != 1 and not self.reconstruct_amplitude:
+					self.current_reconstruction = np.angle(np.exp(1j * self.phase_amplification * self.current_reconstruction))
+
 				self.current_reconstruction_grayscale = self.grayscale_convert(self.current_reconstruction)
 
 			surface_phase_image = pg.transform.smoothscale(pg.surfarray.make_surface(self.current_reconstruction_grayscale), pg.display.get_surface().get_size())
@@ -97,7 +96,7 @@ class GUI:
 			if self.reference_image_wave is not None:
 				pg.draw.rect(self.screen, pg.Color('RED'), [0, 0, *pg.display.get_surface().get_size()], 4)
 
-			for coordinate, message in zip([(5, 5), (5, 25), (5, 45), (5, 85), (5, 105)], [f"Smoothing: {self.hann_smoothing}", f"Amplification: {self.phase_amplification}", f"Buffer: {self.auto_correlation_buffer}", f"Sideband: {self.sideband_quadrant}{' (L)' if self.sideband_lock else ''}", f"Contrast: {np.round(100 * self.fringe_contrast, 2)}%"]):
+			for coordinate, message in zip([(5, 5), (5, 25), (5, 45), (5, 85), (5, 105)], [f"Smoothing: {self.hann_smoothing}", f"Amplification: {self.phase_amplification}", f"Mask: {self.centerband_mask}%", f"Sideband: {self.sideband_area}{' (L)' if self.sideband_lock else ''}", f"Contrast: {np.round(100 * self.fringe_contrast, 2)}%"]):
 				self.font.render_to(self.screen, coordinate, message, pg.Color('RED'))
 
 			pg.display.flip()
@@ -108,31 +107,29 @@ class GUI:
 		img_fft = sfft.fft2(img_CCD)
 		img_fft_shifted = sfft.fftshift(img_fft)
 
-		if self.sideband_quadrant == 'top':
-			img_shift_cropped = img_fft_shifted[:img_fft_shifted.shape[0]//2 - self.auto_correlation_buffer, :]
-		elif self.sideband_quadrant == 'bottom':
-			img_shift_cropped = img_fft_shifted[img_fft_shifted.shape[0]//2 + self.auto_correlation_buffer:, :]
-		elif self.sideband_quadrant == 'left':
-			img_shift_cropped = img_fft_shifted[:, :img_fft_shifted.shape[1]//2 - self.auto_correlation_buffer]
-		elif self.sideband_quadrant == 'right':
-			img_shift_cropped = img_fft_shifted[:, img_fft_shifted.shape[1]//2 + self.auto_correlation_buffer:]
+		if self.sideband_area == 'upper':
+			img_fft_shifted[img_fft_shifted.shape[0] // 2:, :] = 0
+		elif self.sideband_area == 'lower':
+			img_fft_shifted[:img_fft_shifted.shape[0] // 2, :] = 0
+
+		cb_rr, cb_cc = draw.disk(np.asarray(img_fft_shifted.shape) / 2, min(img_fft_shifted.shape) * (self.centerband_mask / 100))
+		img_fft_shifted[cb_rr, cb_cc] = 0
 
 		if not self.sideband_lock:
-			self.sideband_position = np.argwhere(img_fft_shifted == img_shift_cropped.max())[0]
+			self.sideband_position = np.unravel_index(img_fft_shifted.argmax(), img_fft_shifted.shape)
 			self.sideband_distance = np.linalg.norm(np.asarray(img_fft_shifted.shape) / 2 - self.sideband_position)
 
 		self.fringe_contrast = 2 * np.abs(img_fft_shifted[tuple(self.sideband_position)]) / np.abs(img_fft[0, 0])
 
-		cut_out_idx = [[int(np.clip(sb_pos - self.sideband_distance / 6, 0, img_dim - 1)), int(np.clip(sb_pos + self.sideband_distance / 6, 0, img_dim - 1))] for (sb_pos, img_dim) in zip(self.sideband_position, img_fft_shifted.shape)]
-
-		img_cut_out = img_fft_shifted[cut_out_idx[0][0]:cut_out_idx[0][1], cut_out_idx[1][0]:cut_out_idx[1][1]]
+		sb_rr, sb_cc = draw.rectangle(np.asarray(self.sideband_position) - self.sideband_distance / 6, extent=self.sideband_distance / 3)
+		img_cutout = img_fft_shifted[sb_rr, sb_cc]
 
 		if self.hann_smoothing:
-				img_cut_out *= window('hann', img_cut_out.shape)
+				img_cutout *= filters.window('hann', img_cutout.shape)
 
-		padding = np.abs(img_cut_out.shape[0] - self.dimension) // 2
+		padding = np.abs(img_cutout.shape[0] - self.dimension) // 2
 
-		self.object_image_wave = sfft.ifft2(np.pad(img_cut_out, ((padding, padding), (padding, padding)), constant_values=0))
+		self.object_image_wave = sfft.ifft2(np.pad(img_cutout, ((padding, padding), (padding, padding)), constant_values=0))
 		reconstructed_image_wave = self.object_image_wave / self.reference_image_wave if self.reference_image_wave is not None else self.object_image_wave
 
 		return np.abs(reconstructed_image_wave) if self.reconstruct_amplitude else np.angle(reconstructed_image_wave)
