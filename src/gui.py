84fc4877324d943b8fb1db546412c58dd16a8071
hueseyincelik <104273133+hueseyincelik@users.noreply.github.com>
2024-06-01 22:08:43 +0200

    implement optional phase unwrapping

--- src/gui.py
+++ src/gui.py
@@ -1,4 +1,4 @@
-from skimage import draw
+from skimage import draw, restoration
 import scipy.fft as sfft
 import numpy as np
 
@@ -39,6 +39,8 @@ class GUI:
 		self.fringe_contrast = 0
 
 		self.reconstruct_amplitude = False
+		self.unwrap_phase = False
+
 		self.pause = False
 
 		pg.init()
@@ -75,10 +77,15 @@ class GUI:
 
 					if event.key == pg.K_TAB:
 						self.reconstruct_amplitude = not self.reconstruct_amplitude
+						self.unwrap_phase = False
+
 						self.amplifications, self.phase_amplification = it.islice(it.cycle([1, 2, 3, 4]), 1, None), 1
 
 					if event.key == pg.K_a and not self.reconstruct_amplitude:
 						self.phase_amplification = next(self.amplifications)
+
+					if event.key == pg.K_u and not self.reconstruct_amplitude:
+						self.unwrap_phase = not self.unwrap_phase
 
 					if event.key == pg.K_r:
 						self.reference_image_wave = self.object_image_wave.copy()
@@ -105,6 +112,9 @@ class GUI:
 				if self.phase_amplification != 1 and not self.reconstruct_amplitude:
 					self.current_reconstruction = np.angle(np.exp(1j * self.phase_amplification * self.current_reconstruction))
 
+				if self.unwrap_phase and not self.reconstruct_amplitude:
+					self.current_reconstruction = restoration.unwrap_phase(self.current_reconstruction)
+
 				self.current_reconstruction_grayscale = image.grayscale_convert(self.current_reconstruction)
 
 			surface_phase_image = pg.transform.smoothscale(pg.surfarray.make_surface(self.current_reconstruction_grayscale), pg.display.get_surface().get_size())
@@ -113,8 +123,11 @@ class GUI:
 			if self.reference_image_wave is not None:
 				pg.draw.rect(self.screen, pg.Color('RED'), [0, 0, *pg.display.get_surface().get_size()], 4)
 
-			for coordinate, message in zip([(5, 5), (5, 25), (5, 45), (5, 85), (5, 105)], [f"Filter: {self.butterworth_filter} ({np.round(self.butterworth_cutoff, 2)})", f"Amplification: {self.phase_amplification}", f"Mask: {self.centerband_mask}%", f"Sideband: {self.sideband_area}{' (L)' if self.sideband_lock else ''}", f"Contrast: {np.round(100 * self.fringe_contrast, 2)}%"]):
-				self.font.render_to(self.screen, coordinate, message, pg.Color('RED'))
+			self.font.render_to(self.screen, (5, 5), f"Filter: {self.butterworth_filter} ({np.round(self.butterworth_cutoff, 2)})", pg.Color('RED'))
+			self.font.render_to(self.screen, (5, 25), f"Phase: {self.phase_amplification}x{' (U)' if self.unwrap_phase else ''}", pg.Color('RED'))
+			self.font.render_to(self.screen, (5, 45), f"Mask: {self.centerband_mask}%", pg.Color('RED'))
+			self.font.render_to(self.screen, (5, 85), f"Sideband: {self.sideband_area}{' (L)' if self.sideband_lock else ''}", pg.Color('RED'))
+			self.font.render_to(self.screen, (5, 105), f"Contrast: {np.round(100 * self.fringe_contrast, 2)}%", pg.Color('RED'))
 
 			pg.display.flip()
 
